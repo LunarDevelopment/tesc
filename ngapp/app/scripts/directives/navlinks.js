0056cc9ee5769665ce369da19655563c5424a8e5
LunarDevelopment <development.lunar@gmail.com>
2015-09-24 11:16:13 +0100

    cleaning.

--- ngapp/app/scripts/directives/navlinks.js
+++ ngapp/app/scripts/directives/navlinks.js
@@ -12,12 +12,12 @@ angular.module('tweadsApp')
       restrict: 'E',
       template:       '<li class=""><a title="Tweads Home Page" href="#/">Home</a></li>' +
       '<li class=""><a title="About Social Media Marketing" href="#/about">About</a></li>' +
-      '<li class="" ng-show="!authenticated"><a title="About Social Media Marketing" href="#/signup">Sign Up</a> </li>' +
-      '<li class="" ng-show="authenticated"><a title="About Social Media Marketing" ng-click="logout();">Log Out</a></li>' +
+      '<li class="" ng-show="!authenticated"><a title="Sign up to Tweads" href="#/signup">Sign Up</a> </li>' +
+      '<li class="" ng-show="authenticated"><a title="Log out of Tweads" ng-click="logout();">Log Out</a></li>' +
       '<li class=""  ng-show="authenticated"><a title="About Social Media Marketing" href="#/profile">Profile</a> </li>' +
       '<li class="" ng-show="authenticated"><a title="About Social Media Marketing" href="#/dashboard">Dashboard</a> </li>' +
       '<li class=""  ng-show="authenticated"><a title="About Social Media Marketing" href="#/pay">Pay</a> </li>' +
-      '<li class="" ng-show="!authenticated"><a href="#/login">Log In</a> </li>' +
+      '<li class="" ng-show="!authenticated"><a title="Log in to Tweads" href="#/login">Log In</a> </li>' +
       '<li class=""><a title="Contact Tweads" href="#/contact">Contact</a> </li>' 
     };
   });
